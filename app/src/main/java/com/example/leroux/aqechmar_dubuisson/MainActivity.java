9a0523407dba8cafb939cd5811aaa3a97074daac
guillaume.dubuisson@etu.univ-poitiers.fr <Thiefaine50>
2018-10-13 18:48:06 +0200

    Création de la deuxieme activité

--- app/src/main/java/com/example/leroux/aqechmar_dubuisson/MainActivity.java
+++ app/src/main/java/com/example/leroux/aqechmar_dubuisson/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.leroux.aqechmar_dubuisson;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent action = new Intent(this,DetailActivity.class);
+        startActivity(action);
+        // Do something in response to button
+    }
+
+
 }
