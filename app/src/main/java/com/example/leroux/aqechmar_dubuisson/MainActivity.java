8a8c4dcc166604c511e15d2a99b26f9e62b1e7e0
guillaume.dubuisson@etu.univ-poitiers.fr <Thiefaine50>
2018-10-14 13:06:07 +0200

    Toast de bienvenue

--- app/src/main/java/com/example/leroux/aqechmar_dubuisson/MainActivity.java
+++ app/src/main/java/com/example/leroux/aqechmar_dubuisson/MainActivity.java
@@ -5,20 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Toast bienvenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init_layout();
+        afficheToast();
     }
 
     private void init_layout() {
         for (EnumConstraintLayout ecl : EnumConstraintLayout.values()) { //ecl = enum constraint layout
             findViewById(ecl.layout).setBackground(getDrawable(ecl.drawable));
         }
+    }
+
+    private void afficheToast() {
+        bienvenu = Toast.makeText(this, "Bienvenu", Toast.LENGTH_LONG);
+        bienvenu.show();
     }
 
     /**
@@ -28,13 +37,13 @@ public class MainActivity extends AppCompatActivity {
         Intent action = new Intent(this, DetailActivity.class);
         Log.d("app", "HEYO " + view.getId());
         Log.d("app", "HEYO2 " + R.id.cLMedic1);
-        String test ="";
+        String test = "";
         for (EnumConstraintLayout ecl : EnumConstraintLayout.values()) {
             if (view.getId() == ecl.layout) {
                 test = ecl.name();
             }
         }
-        action.putExtra("recup",test);
+        action.putExtra("recup", test);
         startActivity(action);
     }
 
