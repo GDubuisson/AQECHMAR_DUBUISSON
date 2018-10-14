3e65e00822da40eade7ecc551005c88fdf28bcd5
guillaume.dubuisson@etu.univ-poitiers.fr <Thiefaine50>
2018-10-14 21:04:45 +0200

    Ajout notification

--- app/src/main/java/com/example/leroux/aqechmar_dubuisson/DetailActivity.java
+++ app/src/main/java/com/example/leroux/aqechmar_dubuisson/DetailActivity.java
@@ -1,13 +1,15 @@
 package com.example.leroux.aqechmar_dubuisson;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,20 @@ public class DetailActivity extends AppCompatActivity {
         image.setBackground(getDrawable(enumRecup.drawable));
         titre.setText(enumRecup.name);
         desc.setText(enumRecup.description);
+    }
 
+    public void createNotification(View view) {
+        String test ="";
+        for (EnumConstraintLayout ecl : EnumConstraintLayout.values()) {
+            if (view.getId()==ecl.layout) {
+                test=ecl.name;
+            }
+        }
+        EnumConstraintLayout enumRecup = EnumConstraintLayout.valueOf(test);
+        NotificationCompat.Builder notification=new NotificationCompat.Builder(this,"maNotif")
+                .setSmallIcon(enumRecup.drawable)
+                .setContentTitle(enumRecup.name)
+                .setContentText(enumRecup.description)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
     }
 }
