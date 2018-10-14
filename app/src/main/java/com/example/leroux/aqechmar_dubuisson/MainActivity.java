c40e21810a95c10e22f610eda78480c74cc93be9
guillaume.dubuisson@etu.univ-poitiers.fr <Thiefaine50>
2018-10-14 13:58:17 +0200

    correction URL Menu

--- app/src/main/java/com/example/leroux/aqechmar_dubuisson/MainActivity.java
+++ app/src/main/java/com/example/leroux/aqechmar_dubuisson/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.leroux.aqechmar_dubuisson;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -47,5 +50,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(action);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Uri uri = Uri.parse("http://www.google.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
